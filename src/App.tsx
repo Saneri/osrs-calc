6ab195250b51415475683ca11199c52076938ff1
Saneri <santeri.suitiala@gmail.com>
2024-08-17 20:39:20 +0300

    fix typo, base path, and page title

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,13 @@ import {
 } from "./math/probabilityCalculations";
 
 function App() {
-  const [trials, setTrials] = useState<number | null>(null);
+  const [successes, setSuccesses] = useState<number | null>(null);
   const [probabilities, setProbabilities] = useState<ProbabilityResults | null>(
     null
   );
 
   const onSubmit = (values: FormValues) => {
-    setTrials(values.trials);
+    setSuccesses(values.successes);
     setProbabilities(
       calculateProbabilities(
         values.trials,
@@ -31,27 +31,27 @@ function App() {
         <div>
           <h2>Probability of:</h2>
           <h2>
-            exactly {trials !== null ? trials : "n/a"} successes:{" "}
+            exactly {successes !== null ? successes : "n/a"} successes:{" "}
             {probabilities.exactlyXProbability !== null
-              ? `${(probabilities.exactlyXProbability * 100).toFixed(2)}%`
+              ? `${(probabilities.exactlyXProbability * 100).toFixed(10)}%`
               : "n/a"}
           </h2>
           <h2>
-            at least {trials !== null ? trials : "n/a"} successes:{" "}
+            at least {successes !== null ? successes : "n/a"} successes:{" "}
             {probabilities.atLeastXProbability !== null
-              ? `${(probabilities.atLeastXProbability * 100).toFixed(2)}%`
+              ? `${(probabilities.atLeastXProbability * 100).toFixed(10)}%`
               : "n/a"}
           </h2>
           <h2>
-            less than {trials !== null ? trials : "n/a"} successes:{" "}
+            less than {successes !== null ? successes : "n/a"} successes:{" "}
             {probabilities.lessThanXProbability !== null
-              ? `${(probabilities.lessThanXProbability * 100).toFixed(2)}%`
+              ? `${(probabilities.lessThanXProbability * 100).toFixed(10)}%`
               : "n/a"}
           </h2>
           <h2>
-            more than {trials !== null ? trials : "n/a"} successes:{" "}
+            more than {successes !== null ? successes : "n/a"} successes:{" "}
             {probabilities.moreThanXProbability !== null
-              ? `${(probabilities.moreThanXProbability * 100).toFixed(2)}%`
+              ? `${(probabilities.moreThanXProbability * 100).toFixed(10)}%`
               : "n/a"}
           </h2>
         </div>
